0feba1cf346cde52449ea165dd41eb824ee8e9d1
Unknown <g.r.nobles@rug.nl>
2018-06-15 17:22:25 +0300

    corrections to the storeapp initiation

--- kaymakci/urls.py
+++ kaymakci/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+import store.views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 
